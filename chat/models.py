c004d2115428154fd800eb04eec4f61d09686798
amirrezanadali01 <amirrezanadali2001@gmail.com>
2024-01-01 00:18:34 +0330

    create model

---
+++ chat/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class ChatModel(models.Model):
+    text = models.TextField()
+    sender = models.ForeignKey(User, on_delete = models.CASCADE , related_name='sender')
+    receiver = models.ForeignKey(User , on_delete=models.CASCADE , related_name = 'receiver')
+    date = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default = False)
+
